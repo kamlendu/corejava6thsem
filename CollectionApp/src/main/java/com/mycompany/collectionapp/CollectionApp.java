cff2ba6de068980f0adac00d4f87cc27c7944221
kamlendu <kamlendup@gmail.com>
2025-01-27 14:46:25 +0530

    java

--- CollectionApp/src/main/java/com/mycompany/collectionapp/CollectionApp.java
+++ CollectionApp/src/main/java/com/mycompany/collectionapp/CollectionApp.java
@@ -6,7 +6,14 @@ package com.mycompany.collectionapp;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Vector;
 
 /**
  *
@@ -17,13 +24,125 @@ public class CollectionApp {
     public static void main(String[] args) {
         
         CollectionApp app = new CollectionApp();
-        app.arrayListOps();
+        app.treeMapOps();
         
         System.out.println("Hello World!");
     }
+     
+     void treeMapOps()
+    {
+        TreeMap<Object,Object> list = new TreeMap<>();
+        
+        list.put(1,"Pratham");
+        list.put(2,"Rakesh");
+        list.put(3,"Alka");
+      list.put( 7,"Swati");
+        list.put( 4,556.09);
+        list.put( 5,"Prakash");
+        
+       // list.remove(4);
+        for(Map.Entry e : list.entrySet())
+        {
+            System.out.println("Key "+ e.getKey() + " Value "+ e.getValue());
+        }
+        
+        
+    }
+    
+    void hashMapOps()
+    {
+        HashMap<Object,Object> list = new HashMap<>();
+        
+        list.put(1,"Pratham");
+        list.put("2","Rakesh");
+        list.put(3,"Alka");
+      
+        list.put( 3,556.09);
+        list.put( 5,"Prakash");
+        
+       // list.remove(4);
+        for(Map.Entry e : list.entrySet())
+        {
+            System.out.println("Key "+ e.getKey() + " Value "+ e.getValue());
+        }
+        
+        
+    }
+    
+    
+    void treeSetOps()
+    {
+        TreeSet<String> list = new TreeSet<>();
+     
+        list.add("Pratham");
+        list.add("Rakesh");
+        list.add("Alka");
+         list.add("Alka");
+        list.add( "Swayam");
+        list.add( "Prakash");
+        
+        for(String s : list)
+        {
+            System.out.println(s);
+        }
+        
+    }
+    
+    void hashSetOps()
+    {
+        HashSet<String> list = new HashSet<>();
+     
+        list.add("Pratham");
+        list.add("Rakesh");
+        list.add("Alka");
+         list.add("Alka");
+        list.add( "Swayam");
+        list.add( null);
+        
+        for(String s : list)
+        {
+            System.out.println(s);
+        }
+        
+    }
+    
+   
+    
+       void vectorOps()
+{
+    Comparator c =null;
+    Vector list = new Vector();
+    
+    list.add("Pratham");
+    list.add("Rakesh");
+    list.add("Alka");
+    list.add( "Swayam");
+    list.add( "Prakash");
+    
+    for(int i =0; i< list.size();i++)
+        System.out.println(list.get(i));
+    
+    
+    list.sort(c);
+    
+    for(Object s : list)
+    {
+        System.out.println("-- "+ s.toString());
+    }
+    
+    Iterator it = list.iterator();
+    
+    while(it.hasNext())
+    {
+        System.out.println(it.next());
+    }
+    
+    
+}
 
-
-void arrayListOps()
+    
+    
+    void arrayListOps()
 {
     Comparator c =null;
     ArrayList<String> list = new ArrayList<>();
@@ -51,6 +170,53 @@ void arrayListOps()
     {
         System.out.println(it.next());
     }
+    
+    
+}
+
+
+void linkedListOps()
+{
+    Comparator c =null;
+    LinkedList<String> list = new LinkedList<>();
+    
+    list.add(0,"Pratham");
+    list.add(1,"Rakesh");
+    list.add(2,"Alka");
+    list.add( 3,"Swayam");
+    list.add( 4,"Prakash");
+    
+    list.offerFirst("Jayesh");
+    list.offerLast("Suresh");
+    
+    System.out.println(list.peek());
+    
+    System.out.println(list.peekLast());
+    
+  // Iterator it1 = list.descendingIterator();
+    Iterator it = list.iterator();
+   while(it.hasNext())
+    {
+        System.out.println(it.next());
+    }
+    
+//    for(int i =0; i< list.size();i++)
+//        System.out.println(list.get(i));
+//    
+//    
+//    list.sort(c);
+//    
+//    for(String s : list)
+//    {
+//        System.out.println("-- "+ s);
+//    }
+    
+//    Iterator it = list.iterator();
+//    
+//    while(it.hasNext())
+//    {
+//        System.out.println(it.next());
+//    }
     
     
 }
